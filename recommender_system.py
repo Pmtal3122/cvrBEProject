00cf3598e2275a6d12f81dbbd950ce82df5c8ade
Pmtal3122 <talegaonkar.piyush@gmail.com>
2024-03-29 20:15:03 +0530

    Removed comments from recommender_system.py

--- recommender_system.py
+++ recommender_system.py
@@ -3,8 +3,6 @@ import operator
     
 def recommend(word, data, word_vectors):
     vals = list(data.values())
-    # print("vals:")
-    # print(vals)
     maxAvg=0
     maxInd = -1
     ind = 0
@@ -12,12 +10,10 @@ def recommend(word, data, word_vectors):
     for val in vals:
         avg = 0
         vals_length = 0
-        # print(val)
         for value in val.items():
             try:
                 avg = avg + (value[1] * cosine_similarity([word_vectors[word]], [word_vectors[value[0]]])[0])
                 vals_length = vals_length + value[1]
-                # indices.update({cosine_similarity([word_vectors[word]], [word_vectors[value]])[0], ind})
             except:
                 continue
         avg = avg / vals_length
@@ -26,12 +22,7 @@ def recommend(word, data, word_vectors):
             maxInd = ind
             maxAvg = avg
         ind = ind + 1
-    # print("The indices dict is as follows")
-    # print(indices)
     sorted_indices = dict(sorted(indices.items(), key=operator.itemgetter(1), reverse=True))
-    # print("The sorted indices dict is as follows")
-    # print(sorted_indices)
-    
     indices = {key: sorted_indices[key] for key in list(sorted_indices)[:3]}
     print("The top 3 indices are")
     print(indices)
